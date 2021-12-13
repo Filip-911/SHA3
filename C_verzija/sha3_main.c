e4578c569bcb1c0a3ae9f548001763751a4fe8b6
Filip-911 <filiphnovi29@gmail.com>
2021-12-13 23:28:03 +0100

    one file

--- C_verzija/sha3_main.c
+++ C_verzija/sha3_main.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-#include "sha3_functions.h"
+#include <stdint.h>
 
 #define WORD_SIZE 64
-#define RATE (512/WORD_SIZE)       //  12
-#define CAPACITY (1088/WORD_SIZE)    // +13
-#define SIZE (1600/WORD_SIZE)       // =25
-#define OUTPUT_SIZE (256/WORD_SIZE)
+#define RATE 1088     
+#define CAPACITY 512  
+#define SIZE 1600   
+#define OUTPUT_SIZE 256
 
-#define MATRIX_DIM 5    
-char hex_value(uint64_t , char* hex);
+#define KECCAKF_ROUNDS 24
+#define MATRIX_DIM 5
 
 /*
     SIZE = 25 * 2^l; 
@@ -19,30 +18,14 @@ char hex_value(uint64_t , char* hex);
     keccak_f rounds = 12 + 2*l;
 */
 
-int pad(char *input, int length)
-{    
-    int a;
-    if(length + 1 == RATE)
-    {
-      input = (char*)realloc(input, length + 2);
-      input[length - 2] = 0x81;
-      input[length - 1] = '\0';
-    }
-    else 
-    {
-        a = length/RATE;
-	a++;
-	input = (char*)realloc(input, a*RATE + 1); 
-        input[length] = 0x80;
-        for( int i = length + 1; i < a*RATE - 2; i++)
-        {
-            input[i] = 0x00;
-        }
-    input[a*RATE - 2] = 0x01;
-    input[a*RATE - 1] = '\0';
-    }
-    return a;
-}
+char hex_value(uint64_t , char* hex);
+uint64_t rotate_left(uint64_t a, const uint64_t rotc);
+int pad(int length);
+void keccak_f();
+void absorb(char* input);
+
+uint64_t state[MATRIX_DIM][MATRIX_DIM];
+uint64_t block[MATRIX_DIM][MATRIX_DIM];
 
 int main(int argc, char *argv[]){
   
@@ -52,26 +35,7 @@ int main(int argc, char *argv[]){
         return 0;
     }
 
-        // constants
-        const uint64_t keccakf_rndc[24] = {
-            0x800f008900000001, 0x00a0000f00890082, 0x0030a0a00f0089ff,
-            0x000a00d0f0089080, 0x00d000f00890a08b, 0x040f00890000f0f1,
-            0x00c000f0089000a1, 0x00500f00890f0089, 0x00500f008900c08a,
-            0x00d000f0089000af, 0x00a000f008900d09, 0x000200200f00890a,
-            0x00f0000f008900cb, 0x00f000f00890d088, 0x0050002080f0089f,
-            0x000a0000f0089083, 0x00d00000f00890f2, 0x00006000a00f0089,
-            0x0d0000f00a0f0089, 0x00c00000f008938a, 0x00200400c10f0089,
-            0x0000b000f00f0089, 0x00f0089dc00000c1, 0x08004300b0f00898
-        };
-        const uint64_t keccakf_rotc[25] = {
-            1,  3,  6,  10, 15, 21, 28, 36, 45, 55, 2,  14,
-            27, 41, 56, 8,  25, 43, 62, 18, 39, 61, 20, 44
-        };
-         uint64_t temp;
-         uint64_t B[5][5];
-         uint64_t C[5];
-         uint64_t D[5];
-         uint64_t pi[1][1];
+         int p;
     /*
     TO DO 
     ABSORB
@@ -93,69 +57,173 @@ int main(int argc, char *argv[]){
     int inputLen = strlen(argv[1]);
     
     //concatenate all the args to the input string
-    char *input = (char*)malloc(inputLen);
+    char* input = (char*)malloc(inputLen);
     strcpy(input, argv[1]);
 
     for(int i = 2; i < argc; i++)
       {
-	    inputLen += strlen(argv[i]) + 1; 
+	    inputLen += strlen(argv[i]) + 1;
         input = (char*)realloc(input, inputLen);
         strcat(input, " ");
         strcat(input, argv[i]);
       }
     
     //pad the input using the pad function
-    int blockCount = pad(input,inputLen);
-  
-    // Napravi niz matrica 5x5 i postavi prvih RATE clanova na one iz input stringa
-    uint64_t blocks[blockCount][MATRIX_DIM][MATRIX_DIM];
-    int p=0;
-    for (int i = 0; i < blockCount; i++)
+    int a;
+    if(inputLen == RATE/8 - 1)
     {
-        for (int j = 0; j < MATRIX_DIM; j++)
-        {
-            for (int k = 0; k < MATRIX_DIM; k++)
-            {
-                if(p < RATE)
-                {
-                    blocks[i][j][k] = input[i*RATE + p]; //
-                    p++; //broji koliko clanova je unijeto
-                }
-                else
-                {   
-                    blocks[i][j][k] = 0x0000000000000000; //ostali bajtovi idu na 0
-                }
-            }
-        }
-        p=0;
+      input = (char*)realloc(input, inputLen + 2);
+      input[inputLen - 2] = 0x81;
+      input[inputLen - 1] = '\0';
     }
-    free(input);
+    else
+    {
+        a = inputLen/(RATE/8);
+        a++;
+        input = (char*)realloc(input, a*RATE/8 + 1);
+        input[inputLen] = 0x80;
+        for( int i = inputLen + 1; i < a*RATE/8 - 2; i++)
+        {
+            input[i] = 0x00;
+        }
+        input[a*RATE/8 - 2] = 0x01;
+        input[a*RATE/8 - 1] = '\0';
+    }
+    int blockCount = a;
+
+    while(*input)
+    {
+        unsigned int ex = 0;
+        ex += *input;
+        printf("input hex : %02x\n", ex);
+        input++;
+    }
 
     // initialize the state S to a inputing of b zero bits
-     uint64_t state[MATRIX_DIM][MATRIX_DIM];
+    uint64_t state[MATRIX_DIM][MATRIX_DIM];
     for(int i = 0; i < MATRIX_DIM; i++)
         for (int k = 0; k < MATRIX_DIM; k++)
             state[i][k] = 0x0000000000000000;
 
     /*absorb the input into the state: 
     for each block Pi:
-    extend Pi at the end by a inputing of CAPACITY (17) zero bits, yielding one of length SIZE*/
-    //DONE
+    extend Pi at the end by a inputing of CAPACITY zero bits, yielding one of length SIZE
+    XOR that with state
+    apply the block permutation f to the result, yielding a new state S and do it until you run out of blocks to xor with*/
 
-    //XOR that with state
-    //apply the block permutation f to the result, yielding a new state S and do it until you run out of blocks to xor with
     for (int i = 0; i < blockCount; i++)
     {
+        absorb(&input[i*RATE/8]); //absorbing done into the block matrix
         for (int j = 0; j < MATRIX_DIM; j++)
         {
             for (int k = 0; k < MATRIX_DIM; k++)
             {
-                state[j][k] ^= blocks[i][j][k];
+                state[j][k] ^= block[j][k];
             }
         }
+        keccak_f();
+    }
+    free(input);
+
+    //initialize Z to be the empty string
+    uint64_t z[OUTPUT_SIZE/WORD_SIZE + 1];
+    for (size_t i = 0; i < OUTPUT_SIZE/WORD_SIZE; i++)
+    {
+        z[i] = 0x0000000000000000;
+    }
+    z[OUTPUT_SIZE/WORD_SIZE] = '\0';
+    
+    //while the length of Z is less than OUTPUT_SIZE:
+    //append the first RATE bits of STATE to Z
+    p=0;
+    for (size_t i = 0; i < MATRIX_DIM; i++)
+    {
+        for (size_t j = 0; j < MATRIX_DIM; j++)
+        {
+            z[p++] = state[i][j];
+            if(p == OUTPUT_SIZE/WORD_SIZE)
+                break;
+        }
+        if(p == OUTPUT_SIZE/WORD_SIZE)
+            break;
+    }
+
+    int hex_amount =  OUTPUT_SIZE / 8 * 2 + 1;
+    char hex[hex_amount];
+    for (size_t i = 0; i < OUTPUT_SIZE/WORD_SIZE; i++)
+    {
+        hex_value(z[i], &hex[i * WORD_SIZE/8 *2]);
+    }
+    hex[hex_amount - 1] = '\0';
+
+    printf("\nSha3 digest: 0x%s\n\n", hex);
+    for (size_t i = 0; i < hex_amount; i++)
+    {
+        printf("%c", hex[i]);
+    }
+    printf("\n\n");
+    for (size_t i = 0; i < OUTPUT_SIZE/WORD_SIZE; i++)
+    {
+        printf("%lu  ", z[i]);
+    }
+    printf("\n\n");
+    
+    //if Z is still less than d bits long, apply f to S, yielding a new state S
+    //truncate Z to d bits
+    //no need for that ^
+    //                 |
+    return 0;
+}
+/*
+int pad(int length)
+{    
+    int a;
+    if(length == RATE/8 - 1)
+    {
+      input = (char*)realloc(input, length + 2);
+      input[length - 2] = 0x81;
+      input[length - 1] = '\0';
+    }
+    else
+    {
+        a = length/RATE/8;
+        a++;
+        input = (char*)realloc(input, a*RATE/8 + 1); 
+        input[length] = 0x80;
+        for( int i = length + 1; i < a*RATE/8 - 2; i++)
+        {
+            input[i] = 0x00;
+        }
+        input[a*RATE/8 - 2] = 0x01;
+        input[a*RATE/8 - 1] = '\0';
+    }
+    return a;
+}*/
+void keccak_f()
+{
+        // constants
+        const uint64_t keccakf_rndc[24] = {
+            0x0000000000000001, 0x0000000000008082, 0x800000000000808a,
+            0x8000000080008000, 0x000000000000808b, 0x0000000080000001,
+            0x8000000080008081, 0x8000000000008009, 0x000000000000008a,
+            0x0000000000000088, 0x0000000080008009, 0x000000008000000a,
+            0x000000008000808b, 0x800000000000008b, 0x8000000000008089,
+            0x8000000000008003, 0x8000000000008002, 0x8000000000000080,
+            0x000000000000800a, 0x800000008000000a, 0x8000000080008081,
+            0x8000000000008080, 0x0000000080000001, 0x8000000080008008
+        };
+        const uint64_t keccakf_rotc[25] = {
+            1,  3,  6,  10, 15, 21, 28, 36, 45, 55, 2,  14,
+            27, 41, 56, 8,  25, 43, 62, 18, 39, 61, 20, 44
+        };
+         uint64_t temp;
+         uint64_t B[5][5];
+         uint64_t C[5];
+         uint64_t D[5];
+         uint64_t pi[1][1];
+         
         for(int n = 0; n < KECCAKF_ROUNDS; n++)
         {
-
             //theta
             for (int x = 0; x < MATRIX_DIM; x++)
             {
@@ -163,29 +231,20 @@ int main(int argc, char *argv[]){
                 for (int y = 1; y < MATRIX_DIM; y++)
                     C[x] ^= state[x][y];
             }
-
             for (int x = 0; x < MATRIX_DIM; x++)
             {
-                D[x] = C[(x - 1) % MATRIX_DIM] ^ rotate_right(C[(x + 1) % MATRIX_DIM], 1);
+                D[x] = C[(x - 1) % MATRIX_DIM] ^ rotate_left(C[(x + 1) % MATRIX_DIM], 1);
                 for (int y = 0; y < MATRIX_DIM; y++)
                 {
                     state[x][y] ^= D[x];
                 }
             }
-
-            //chi
-            for (int x = 0; x < MATRIX_DIM; x++)
-            {
-                for (int y = 0; y < MATRIX_DIM; y++)
-                    state[x][y] = state[x][y] ^ (~state[(x + 1) % MATRIX_DIM][y] & state[(x + 2) % MATRIX_DIM][y]);
-            }
-
             //rho
             int x = 1;
             int y = 0;
             for(int t = 0; t < 24; t++)
             {
-                state[x][y] = rotate_right(state[x][y], keccakf_rotc[t]);
+                state[x][y] = rotate_left(state[x][y], keccakf_rotc[t]);
 
                 temp = x;
                 x = y;
@@ -204,62 +263,38 @@ int main(int argc, char *argv[]){
                     state[x][y] = pi[0][0];
                 }
             }
-
+            //chi
+            for (int x = 0; x < MATRIX_DIM; x++)
+            {
+                for (int y = 0; y < MATRIX_DIM; y++)
+                    state[x][y] = state[x][y] ^ (~state[(x + 1) % MATRIX_DIM][y] & state[(x + 2) % MATRIX_DIM][y]);
+            }
             //iota
             //arbitrary lane in the state is xored with the constants
                state[0][0] ^= keccakf_rndc[n];
         }
+}
 
-    }
-
-    //initialize Z to be the empty string
-    uint64_t z[OUTPUT_SIZE + 1];
-    for (size_t i = 0; i < OUTPUT_SIZE; i++)
-    {
-        z[i] = 0x0000000000000000;
-    }
-    z[OUTPUT_SIZE] = '\0';
-    
-    //while the length of Z is less than OUTPUT_SIZE:
-    //append the first RATE bits of STATE to Z
-    p=0;
+void absorb(char* input)        
+{
+    int p = 0;
     for (size_t i = 0; i < MATRIX_DIM; i++)
     {
         for (size_t j = 0; j < MATRIX_DIM; j++)
         {
-            z[p++] = state[i][j];
-            if(p == OUTPUT_SIZE)
-                break;
+            if(p < RATE/8)
+            {
+                for (size_t k = 0; k < 8; k++)
+                {
+                    block[i][j] += input[i*5*WORD_SIZE/8 + j*WORD_SIZE/8 + k];
+                    block[i][j] <<= 8;
+                }
+                p += 8;
+            }
+            else
+                block[i][j] = 0x0000000000000000;
         }
-        if(p == OUTPUT_SIZE)
-            break;
     }
-
-    int hex_amount =  OUTPUT_SIZE * WORD_SIZE / 8 * 2 + 1;
-    char hex[hex_amount];
-    for (size_t i = 0; i < OUTPUT_SIZE; i++)
-    {
-        hex_value(z[i], &hex[i * WORD_SIZE/8 *2]);
-    }
-    hex[hex_amount - 1] = '\0';
-
-    printf("\nSha3 digest: 0x%s\n", hex);
-    for (size_t i = 0; i < hex_amount; i++)
-    {
-        printf("%c", hex[i]);
-    }
-    printf("\n");
-    for (size_t i = 0; i < OUTPUT_SIZE; i++)
-    {
-        printf("%lu  ", z[i]);
-    }
-    printf("\n");
-    
-    //if Z is still less than d bits long, apply f to S, yielding a new state S
-    //truncate Z to d bits
-    //no need for that ^
-    //                 |
-    return 0;
 }
 
 char hex_value(uint64_t dec_value, char* hex)
@@ -275,5 +310,21 @@ char hex_value(uint64_t dec_value, char* hex)
             hex[15 - i] = 55 + rem;
         tmp /= 16;
     }
-    
+}
+
+uint64_t rotate_left(uint64_t a, const uint64_t rotc)
+{
+    uint64_t rot = rotc % 64;
+    uint64_t tmp;
+    while(rot)
+    {   
+        tmp = a & 0x0000000000000001;
+        a >>= 1;
+        if(tmp)
+            a |= 0x8000000000000000;
+
+        rot--;
+    }
+    return a;
+
 }
